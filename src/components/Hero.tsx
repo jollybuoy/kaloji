6c1342fa4786e599374e75ebe83e40c85d365b6a
jollybuoy <jollybuoytech@gmail.com>
2025-08-26 01:28:59 -0400

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -65,7 +65,7 @@ const Hero: React.FC<HeroProps> = ({ language, onBookingClick }) => {
         {/* Background Image */}
         <div 
           className="absolute inset-0 bg-cover bg-center bg-no-repeat"
-          style={{ backgroundImage: 'url(/FINAL-KV-01.jpg)' }}
+          style={{ backgroundImage: 'url(/67039ffadd4a2-6703a369255db.jpg)' }}
         />
         
         {/* Overlay */}
